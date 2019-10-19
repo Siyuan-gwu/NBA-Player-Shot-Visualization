7b2f8209d38019c39e4895bb49d4a9a4c0f1d211
Siyuan-gwu <imsiyuan.z@gmail.com>
2019-10-19 13:23:53 -0400

    TopNavBar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {TopNavBar} from './TopNavBar.js';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar/>
+    </div>
+  );
+}
+
+export default App;
